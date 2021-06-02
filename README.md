cb66ca0263ed26029e1fee9a1dc9f16c1271c0dc
gouz <sylvain@gougouzian.fr>
2021-06-02 09:38:26 +0200

    [+] fetch

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # pastarr
+
 Automatic wallpaper from cli
 
 Thanks to https://github.com/sindresorhus/wallpaper pastarr is cross-platform (windows, mac, linux)
 
 ## installation
 
-````
+```
+$ npm install -g playwright
 $ npm install -g pastarr
-````
+```
 
 ## use
 
-````
-$ pastarr "assassin's creed"
-````
+```
+$ pastarr -S "assassin's creed"
+
+$ pastarr -S "assassin's creed" -W 1920 -H 1200
+```
 
 ## troubleshooting
 
--- index.js
+++ index.js
@@ -2,60 +2,62 @@
 const { chromium } = require("playwright");
 
 const fs = require("fs");
-const request = require("request");
+const fetch = require("node-fetch");
 const wallpaper = require("wallpaper");
 const homedir = require("homedir");
-const readChunk = require("read-chunk");
-const program = require("commander");
+const { program } = require("commander");
 
 program
-  .version("0.1.0")
-  .usage("[options] search")
-  .option("-W, --width <n>", "width of the screen", 1920, parseInt)
-  .option("-H, --height <n>", "height of the screen", 1080, parseInt)
-  .action((search, options) => {
-    (async () => {
-      const browser = await chromium.launch();
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      let url =
-        "https://www.google.fr/search?q=" +
-        encodeURI(`${search} imagesize:${options.width}x${options.height}`) +
-        "&tbm=isch&source=hp&biw=1920&bih=983";
-      await page.goto(url);
-      await page.evaluate(() => {
-        const arr = document.querySelectorAll("a.wXeWr");
-        arr[Math.floor(Math.random() * arr.length)].click();
-      });
-      await page.waitForSelector('a[rlhc="1"]');
-      const img = await page.evaluate(() => {
-        return document.querySelector('a[rlhc="1"] img').getAttribute("src");
-      });
-      if (img.startsWith("http")) {
-        const path = homedir() + "/pastarr/";
-        try {
-          fs.accessSync(path);
-        } catch (e) {
-          fs.mkdirSync(path);
-        }
-        const ext = ".png";
-        const uniq = new Date().getTime();
-        request.head(img, (err, res, body) => {
-          request(img)
-            .pipe(fs.createWriteStream(path + uniq + ext))
-            .on("close", () => {
-              readChunk.sync(path + uniq + ext, 0, 4100);
-              wallpaper
-                .set(path + uniq + ext, {
-                  scale: "fill",
-                })
-                .then(() => {
-                  console.log("done ! (" + path + uniq + ext + ")");
-                });
-            });
+  .version("0.1.1")
+  .requiredOption("-S, --search <s>", "search")
+  .option("-W, --width <n>", "width of the screen")
+  .option("-H, --height <n>", "height of the screen")
+  .parse();
+
+(async () => {
+  const search = program.opts().search;
+  const width = program.opts().width
+    ? `${parseInt(program.opts().width)} `
+    : 1920;
+  const height = program.opts().height
+    ? `${parseInt(program.opts().height)} `
+    : 1200;
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  let url =
+    "https://www.google.fr/search?q=" +
+    encodeURI(`${search} imagesize:${width}x${height}`) +
+    "&tbm=isch&source=hp&biw=1920&bih=983";
+  await page.goto(url);
+  await page.evaluate(() => {
+    const arr = document.querySelectorAll("a.wXeWr");
+    arr[Math.floor(Math.random() * arr.length)].click();
+  });
+  await page.waitForSelector('a[rlhc="1"]');
+  const img = await page.evaluate(() => {
+    return document.querySelector('a[rlhc="1"] img').getAttribute("src");
+  });
+  if (img.startsWith("http")) {
+    const path = homedir() + "/pastarr/";
+    try {
+      fs.accessSync(path);
+    } catch (e) {
+      fs.mkdirSync(path);
+    }
+    const ext = ".png";
+    const uniq = new Date().getTime();
+    fetch(img).then((res) => {
+      const dest = fs.createWriteStream(path + uniq + ext);
+      res.body.pipe(dest);
+      wallpaper
+        .set(path + uniq + ext, {
+          scale: "fill",
+        })
+        .then(() => {
+          console.log("done ! (" + path + uniq + ext + ")");
         });
-      } else console.log("not found");
-      await browser.close();
-    })();
-  })
-  .parse(process.argv);
+    });
+  } else console.log("not found");
+  await browser.close();
+})();
